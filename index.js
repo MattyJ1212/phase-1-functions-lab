004f24235c1dd090a036e1fee7c75cebb3b3a247
MatthewJacquez <matt.cras@gmail.com>
2023-10-09 23:26:53 -0700

    commit tree 25dc1595859c48e5c16a903e9f505386a9ee9dab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const scruberHq = 42
+
+function distancFromHqInBlocks(pickupLoaction){
+    return distanceFromHqInBlocks(scruberHq > `${pickupLocation} || scruberHq < `${pickupLocation} );
+}
+
+// function distanceFromHqInFeet(someValue){
+//     return distanceFromHqInBlocks(264 * ${someValue})
+// }
+
+// function calculateFarePrice(start,destination){
+//     return numberOfFeetTraveled(feet)
+//     if (feet <= 400) {
+//         return "This one is on me!";
+//     }else if (feet >400 && feet <2000){
+//       return "That will be twenty bucks."
+//     }else if (feet >2000 && feet <2500){
+//       return "I will gladly take your thirty bucks."
+//     } else if (feet >2500){
+//       return "No can do."
+//     }}
